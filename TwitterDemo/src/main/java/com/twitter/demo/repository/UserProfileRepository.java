5943046b3b1d806afb84c13675bcfe3b95ea77fb
OLX <OLX@INODE18373.local>
2021-09-23 19:58:08 +0530

    Cleanup

--- TwitterDemo/src/main/java/com/twitter/demo/repository/UserProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.twitter.demo.repository;
-
-import com.twitter.demo.modal.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile,Long> {
-}
