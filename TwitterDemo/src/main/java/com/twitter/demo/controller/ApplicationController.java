9f1a3cee31926a69d8c916b23f7f8209ffb31dd5
sangita.sengar <sangita.sengar@olx.com>
2021-09-23 19:32:05 +0530

    testing commit

--- TwitterDemo/src/main/java/com/twitter/demo/controller/ApplicationController.java
+++ TwitterDemo/src/main/java/com/twitter/demo/controller/ApplicationController.java
@@ -3,7 +3,7 @@ package com.twitter.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//TestCommit
 @Controller
 public class ApplicationController {
 
