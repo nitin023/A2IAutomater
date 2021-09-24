6cf017069ee7f068b931f8922762fbee365e1106
komal.sharma1 <komal.sharma1@olx.com>
2021-09-23 23:47:53 -0700

    Sms layer

--- TwitterDemo/src/main/java/com/twitter/demo/controller/ApplicationController.java
+++ TwitterDemo/src/main/java/com/twitter/demo/controller/ApplicationController.java
@@ -3,22 +3,30 @@ package com.twitter.demo.controller;
 import com.twitter.demo.Resources.Email.Constant.EmailTemplate;
 import com.twitter.demo.Services.EmailService;
 import com.twitter.demo.modal.Email;
+import com.twitter.demo.sms.service.SmsSender;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class ApplicationController {
 
     @Autowired
     EmailService emailService;
 
-    @ResponseBody
+    @Autowired
+    SmsSender smsSender;
+
     @RequestMapping("/send-email")
     public String sendEmail() {
         Email email = new Email("test email", "komal.sharma1@olx.com", EmailTemplate.APP_VERIFY);
         emailService.setSendMail(email);
         return "Email sent";
+    }
+
+    @RequestMapping("/sendSms/{toPhoneNumber}")
+    public String sendSms(@PathVariable String toPhoneNumber)
+    {
+        smsSender.sendSMS(toPhoneNumber);
+        return "Sms sent";
     }
 }
